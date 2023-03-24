29057d62fea49a4797e7a21738a561ca272ae16a
Schedarian <75801123+Schedarian@users.noreply.github.com>
2023-03-24 15:56:57 +0300

    Update Main.rb

--- Main.rb
+++ Main.rb
@@ -4,41 +4,17 @@ require "json"
 require "net/http"
 require "uri"
 require "fileutils"
-require "caldera"
 require "sqlite3"
 
-require_relative "src/GlobalCommands.rb"
-require_relative "src/ServerCommands.rb"
-require_relative "src/Events.rb"
-require_relative "src/Database.rb"
 require_relative "src/Config.rb"
+require_relative "src/Initializer.rb"
 
 module Rubedo
-  $users = 0
-  bot = Discordrb::Bot.new(token: Config.get_token, intents: :all)
-  caldera = Caldera::Client.new(num_shards: 1, user_id: 840665896556560435, connect: lambda { |gid, cid|
-                                  bot.gateway.send_voice_state_update(gid, cid, false, true)
-                                })
-
+  bot = Discordrb::Bot.new(token: Config::BOT_TOKEN, intents: :all)
   bot.init_cache
-  bot.ready do
+  bot.ready {
     bot.dnd
-    GlobalCommands.register_global_commands(bot)
-    GlobalCommands.handle_global_commands(bot)
-    Commands.handle_all(bot, caldera)
-
-    Thread.new do
-      loop do
-        $users = 0
-        bot.servers.each do |key, value|
-          $users += value.member_count
-        end
-
-        bot.watching = ("#{$users} users")
-        sleep(900)
-      end
-    end
-  end
-
+    Initializer.run(bot)
+  }
   bot.run
 end
