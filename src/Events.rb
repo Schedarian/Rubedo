0abbf3358a30d034d9d8bb0ca605f46cc5318705
Schedarian <75801123+Schedarian@users.noreply.github.com>
2023-03-24 15:57:44 +0300

    Delete src directory

--- src/Events.rb
+++
@@ -1,6 +0,0 @@
-#Initialize all event listeners
-
-module Events
-  def self.init_events(bot)
-  end
-end
--- src/events/ServerJoin.rb
+++
@@ -1,7 +0,0 @@
-module Events
-    def self.server_join(bot)
-        bot.server_create do |event|
-            
-        end
-    end
-end
